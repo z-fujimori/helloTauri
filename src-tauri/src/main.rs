01a082ba5d29c1f6b98591f0792d51bf2992722e
z-fujimori <fuji.tai.29@i.softbank.jp>
2024-07-31 01:39:02 +0900

    チュートリアル終了

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,34 +1,25 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
 
 use serde::{Deserialize, Serialize};
-/// ボード
+use tauri::{Manager, State};
+
+pub(crate) mod database;
+
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Board {
+pub struct Board{
     columns: Vec<Column>,
 }
-/// カラム
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Column {
     id: i64,
     title: String,
     cards: Vec<Card>,
 }
-/// カード
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Card {
-    id: i64,
-    title: String,
-    description: Option<String>,
-}
-/// カードの位置
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CardPos {
-    #[serde(rename = "columnId")]
-    column_id: i64,
-    position: i64,
-}
+
 impl Column {
     pub fn new(id: i64, title: &str) -> Self {
         Column {
@@ -37,78 +28,147 @@ impl Column {
             cards: Vec::new(),
         }
     }
+
     pub fn add_card(&mut self, card: Card) {
         self.cards.push(card);
     }
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Card {
+    id: i64,
+    title: String,
+    description: Option<String>,
+}
+
 impl Card {
-    pub fn new(id: i64, title: &str, description: Option<&str>) -> Self {
+    // descriptionの引数をOption<&str>からOption<String>に変更
+    pub fn new(id: i64, title: &str, description: Option<String>) -> Self {
         Card {
             id,
             title: title.to_string(),
-            description: description.map(ToString::to_string),
+            description,
         }
     }
 }
-// ボードのデータを作成して返すハンドラ
-#[tauri::command]
-fn get_board() -> Result<Board, String> {
-    let mut col0 = Column::new(0, "バックログ");
-    col0.add_card(Card::new(0, "かんばんボードを追加する", Some("react-kanbanを使用する")));
-    let col1 = Column::new(1, "開発中");
-    let board = Board { columns: vec![col0, col1] };
-    Ok(board)
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CardPos {
+    #[serde(rename = "columnId")]
+    column_id: i64,
+    position: i64,
 }
-/// カードの追加直後に呼ばれるハンドラ
+
 #[tauri::command]
-async fn handle_add_card(card: Card, pos: CardPos) -> Result<(), String> {
-    // IPCで受信したデータをデバッグ表示する
-    println!("handle_add_card ----------");
-    dbg!(&card);
-    dbg!(&pos);
-    Ok(())
+async fn get_board(sqlite_pool: State<'_, sqlx::SqlitePool>) -> Result<Board, String> {
+    let columns = database::get_columns(&*sqlite_pool)
+        .await
+        .map_err(|e| e.to_string())?;
+    Ok(Board { columns })
 }
-/// カードの移動直後に呼ばれるハンドラ
+
 #[tauri::command]
-async fn handle_move_card(card: Card, from: CardPos, to: CardPos) -> Result<(), String> {
-    println!("handle_move_card ----------");
-    dbg!(&card);
-    dbg!(&from);
-    dbg!(&to);
-    Ok(())
-}
-/// カードの削除直後に呼ばれるハンドラ
-#[tauri::command]
-async fn handle_remove_card(card: Card, column_id: i64) -> Result<(), String> {
-    println!("handle_remove_card ----------");
-    dbg!(&card);
-    dbg!(&column_id);
+async fn handle_add_card(
+    sqlite_pool: State<'_, sqlx::SqlitePool>,
+    card: Card,
+    pos: CardPos,
+) -> Result<(), String> {
+    database::insert_card(&*sqlite_pool, card, pos)
+        .await
+        .map_err(|e| e.to_string())?;
+
     Ok(())
 }
 
-fn main() {
+#[tauri::command]
+async fn handle_move_card(
+    sqlite_pool: State<'_, sqlx::SqlitePool>,
+    card: Card,
+    from: CardPos,
+    to: CardPos,
+) -> Result<(), String> {
+    database::move_card(&*sqlite_pool, card, from, to)
+        .await
+        .map_err(|e| e.to_string())?;
+
+    Ok(())
+}
+
+#[tauri::command]
+async fn handle_remove_card(
+    sqlite_pool: State<'_, sqlx::SqlitePool>,
+    card: Card,
+    column_id: i64,
+) -> Result<(), String> {
+    database::delete_card(&*sqlite_pool, card, column_id)
+        .await
+        .map_err(|e| e.to_string())?;
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // このmain関数はasync fnではないので、asyncな関数を呼ぶのにblock_on関数を使う
+    use tauri::async_runtime::block_on;
+
+    // データベースのファイルパス等を設定する
+    const DATABASE_DIR: &str = "gihyo-kanban-db";
+    const DATABASE_FILE: &str = "db.sqlite";
+    // ユーザのホームディレクトリ直下にデータベースのディレクトリを作成する
+    // もし、各OSで標準的に使用されるアプリ専用のデータディレクトリに保存したいなら
+    // directoriesクレートの`ProjectDirs::data_dir`メソッドなどを使うとよい
+    // https://docs.rs/directories/latest/directories/struct.ProjectDirs.html#method.data_dir
+    let home_dir = directories::UserDirs::new()
+        .map(|dirs| dirs.home_dir().to_path_buf())
+        // ホームディレクトリが取得できないときはカレントディレクトリを使う
+        .unwrap_or_else(|| std::env::current_dir().expect("Cannot access the current directory"));
+    let database_dir = home_dir.join(DATABASE_DIR);
+    let database_file = database_dir.join(DATABASE_FILE);
+
+    // データベースファイルが存在するかチェックする
+    let db_exists = std::fs::metadata(&database_file).is_ok();
+    // 存在しないなら、ファイルを格納するためのディレクトリを作成する
+    if !db_exists {
+        std::fs::create_dir(&database_dir)?;
+    }
+
+    // データベースURLを作成する
+    //
+    // 2022年12月28日 修正
+    //     Windows環境でも動作するよう、std::fs::canonicalizeを、dunce::canonicalizeに変更
+    //
+    //     stdのcanonicalizeは、Windows環境ではUNCパス（例：\\?\C:\Users\..）を返すが、
+    //     それを元にURLを作成するとSQLiteが受け付けないものになってしまう。dunceの
+    //     canonicalizeは普通のパス（例：C:\Users\）を返すので、それを使えばWindowsでも動く。
+    //
+    //     詳細: https://github.com/tatsuya6502/gihyo-tauri-kanban/pull/1
+    let database_dir_str = dunce::canonicalize(&database_dir)
+        .unwrap()
+        .to_string_lossy()
+        .replace('\\', "/");
+    let database_url = format!("sqlite://{}/{}", database_dir_str, DATABASE_FILE);
+
+    // SQLiteのコネクションプールを作成する
+    let sqlite_pool = block_on(database::create_sqlite_pool(&database_url))?;
+
+    //  データベースファイルが存在しなかったなら、マイグレーションSQLを実行する
+    if !db_exists {
+        block_on(database::migrate_database(&sqlite_pool))?;
+    }
+
     tauri::Builder::default()
-        // ハンドラを登録する。（元々あったgreetハンドラは削除した）
         .invoke_handler(tauri::generate_handler![
             get_board,
             handle_add_card,
             handle_move_card,
             handle_remove_card
         ])
+        // ハンドラからコネクションプールにアクセスできるよう、登録する
+        .setup(|app| {
+            app.manage(sqlite_pool);
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    Ok(())
 }
-
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-// fn main() {
-//     tauri::Builder::default()
-//         .invoke_handler(tauri::generate_handler![greet])
-//         .run(tauri::generate_context!())
-//         .expect("error while running tauri application");
-// }
--- src/App.tsx
+++ src/App.tsx
@@ -1,114 +1,93 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import { invoke } from "@tauri-apps/api/tauri";
-// import "./App.css";
-
+// @ts-ignore
 import Board from '@asseinfo/react-kanban';
-import '@asseinfo/react-kanban/dist/styles.css'
-// Tauriが提供するinvoke関数をインポートする
+import '@asseinfo/react-kanban/dist/styles.css';
+import { useState, useEffect } from 'react';
 import { invoke } from '@tauri-apps/api'
 
-// かんばんボードに最初に表示するデータを作成する
-const board = {
-  columns: [
-    {
-      id: 0,
-      title: 'バックログ',
-      cards: [
-        {
-          id: 0,
-          title: 'かんばんボードを追加する',
-          description: 'react-kanbanを使用する'
-        },
-      ]
-    },
-    {
-      id: 1,
-      title: '開発中',
-      cards: []
-    }
-  ]
-}
-
-// ボードを表す型定義
 type TBoard = {
   columns: [TColumn];
 }
-// カラムを表す型定義
+
 type TColumn = {
   id: number;
   title: string;
   cards: [TCard];
 }
-// カードを表す型定義
+
 type TCard = {
   id: number;
   title: string;
   description: string | undefined;
 }
-// カードの移動元を表す型定義
+
 type TMovedFrom = {
   fromColumnId: number;
   fromPosition: number;
 }
-// カードの移動先を表す型定義
+
 type TMovedTo = {
   toColumnId: number;
   toPosition: number;
 }
-// カードの位置を表すクラス
+
 class CardPos {
   columnId: number;
   position: number;
+
   constructor(columnId: number, position: number) {
     this.columnId = columnId;
     this.position = position;
   }
 }
-// カードの追加直後に呼ばれるハンドラ
-async function handleAddCard(board: TBoard, column: TColumn, card: TCard) {
+
+async function handleAddCard( column: TColumn, card: TCard) {
   const pos = new CardPos(column.id, 0);
-  // IPCでCoreプロセスのhandle_add_cardを呼ぶ（引数はJSON形式）
   await invoke<void>("handle_add_card", { "card": card, "pos": pos })
 };
-// カードの移動直後に呼ばれるハンドラ
-async function handleMoveCard(board: TBoard, card: TCard, from: TMovedFrom, to: TMovedTo) {
+
+async function handleMoveCard( card: TCard, from: TMovedFrom, to: TMovedTo) {
   const fromPos = new CardPos(from.fromColumnId, from.fromPosition);
   const toPos = new CardPos(to.toColumnId, to.toPosition);
   await invoke<void>("handle_move_card", { "card": card, "from": fromPos, "to": toPos })
 }
-// カードの削除直後に呼ばれるハンドラ
-async function handleRemoveCard(board: TBoard, column: TColumn, card: TCard) {
+
+async function handleRemoveCard( column: TColumn, card: TCard) {
   await invoke<void>("handle_remove_card", { "card": card, "columnId": column.id })
 };
 
 function App() {
+  const [board, setBoard] = useState<TBoard | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const board = await invoke<TBoard>("get_board", {})
+        .catch(err => {
+          console.error(err);
+          return null
+        });
+      console.debug(board);
+      setBoard(board);
+    })();
+  }, []);
 
   return (
     <>
-      <Board
-        // ボードの初期データ
-        initialBoard={board}
-        // カードの追加を許可（トップに「＋」ボタンを表示）
-        allowAddCard={{ on: "top" }}
-        // カードの削除を許可
-        allowRemoveCard
-        // カラム（カードのグループ）のドラッグをオフにする
-        disableColumnDrag
-        // 新しいカードの作成時、idに現在時刻の数値表現をセットする
-        onNewCardConfirm={(draftCard: any) => ({
-          id: new Date().getTime(),
-          ...draftCard
-        })}
-        // // 新しいカードが作成されたら、カード等の内容をコンソールに表示する
-        // onCardNew={console.log}
-        // カード等の内容をターミナルに表示する
-        onCardNew={handleAddCard}
-        onCardDragEnd={handleMoveCard}
-        onCardRemove={handleRemoveCard}
-      />
+      {board != null &&
+        <Board
+          initialBoard={board}
+          allowAddCard={{ on: "top" }}
+          allowRemoveCard
+          disableColumnDrag
+          onNewCardConfirm={(draftCard: any) => ({
+            id: new Date().getTime(),
+            ...draftCard
+          })}
+          onCardNew={handleAddCard}
+          onCardDragEnd={handleMoveCard}
+          onCardRemove={handleRemoveCard}
+        />}
     </>
-  );
+  )
 }
 
 export default App;
